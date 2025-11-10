15603822e4bb38722fc40b93e444934aae87acab
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2025-11-10 16:52:06 +0200

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -125,7 +125,9 @@ const forgotPassword = async (req, res, next) => {
 
     try {
       const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
           user: process.env.EMAIL_USERNAME,
           pass: process.env.EMAIL_PASSWORD,
