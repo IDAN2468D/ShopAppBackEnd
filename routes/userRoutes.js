6f3f6c24bd7fc1a3e1bcbb610e3c13d91eb78a0f
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2025-11-10 13:52:04 +0200

    Update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, deleteUser } = require('../controllers/userController');
+const { registerUser, loginUser, deleteUser, forgotPassword, resetPassword } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
