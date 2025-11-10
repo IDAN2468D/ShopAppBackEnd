ccb882603c53ff948c85cc711b357aee35f92524
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2025-11-10 17:42:08 +0200

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,9 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
-const nodemailer = require('nodemailer');
+const axios = require('axios');
 
+// Generate JWT Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
@@ -22,16 +23,13 @@ const registerUser = async (req, res, next) => {
     }
 
     const userExists = await User.findOne({ email });
-
     if (userExists) {
       return res.status(400).json({ message: 'משתמש כבר קיים.' });
     }
 
-    // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create user
     const user = await User.create({
       name,
       email,
@@ -61,7 +59,6 @@ const loginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    // Check for user email
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
@@ -92,7 +89,6 @@ const deleteUser = async (req, res, next) => {
     }
 
     await user.deleteOne();
-
     res.json({ message: 'המשתמש נמחק בהצלחה.' });
   } catch (error) {
     console.error('שגיאה במחיקת משתמש:', error);
@@ -100,7 +96,7 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
-// @desc    Forgot password
+// @desc    Forgot password using MailerLite API
 // @route   POST /api/users/forgotpassword
 // @access  Public
 const forgotPassword = async (req, res, next) => {
@@ -108,46 +104,48 @@ const forgotPassword = async (req, res, next) => {
     const { email } = req.body;
 
     const user = await User.findOne({ email });
-
     if (!user) {
       return res.status(404).json({ message: 'משתמש לא נמצא עם כתובת אימייל זו.' });
     }
 
-    // Get reset token
-    const resetToken = user.getResetPasswordToken();
+    // Generate reset token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+    user.resetPasswordToken = crypto
+      .createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+    user.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // 10 דקות
 
     await user.save({ validateBeforeSave: false });
 
-    // Create reset URL
     const resetURL = `${req.protocol}://${req.get('host')}/api/users/resetpassword/${resetToken}`;
-
     const message = `קיבלת אימייל זה מכיוון שביקשת לאפס את הסיסמה שלך. אנא בקר בקישור הבא: \n\n ${resetURL} \n\n אם לא ביקשת זאת, אנא התעלם מאימייל זה.`;
 
     try {
-      const transporter = nodemailer.createTransport({
-        host: 'smtp.mailerlite.com',
-        port: 587,
-        secure: false, // true for false, false for other ports
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
+      await axios.post(
+        'https://api.mailerlite.com/api/v2/email/send',
+        {
+          to: [user.email],
+          subject: 'איפוס סיסמה',
+          html: `<p>${message}</p>`,
+          from: process.env.EMAIL_FROM,
         },
-      });
-
-      await transporter.sendMail({
-        to: user.email,
-        from: process.env.EMAIL_FROM,
-        subject: 'איפוס סיסמה',
-        text: message,
-      });
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-MailerLite-ApiKey': process.env.EMAIL_PASSWORD,
+          },
+        }
+      );
 
       res.status(200).json({ success: true, data: 'אימייל נשלח בהצלחה.' });
     } catch (error) {
+      // Reset token if sending fails
       user.resetPasswordToken = undefined;
       user.resetPasswordExpire = undefined;
       await user.save({ validateBeforeSave: false });
 
-      console.error('שגיאה בשליחת אימייל איפוס סיסמה:', error);
+      console.error('שגיאה בשליחת אימייל איפוס סיסמה:', error.response?.data || error.message);
       return res.status(500).json({ message: 'שגיאה בשליחת אימייל איפוס סיסמה.' });
     }
   } catch (error) {
@@ -160,8 +158,10 @@ const forgotPassword = async (req, res, next) => {
 // @access  Public
 const resetPassword = async (req, res, next) => {
   try {
-    // Get hashed token
-    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resettoken).digest('hex');
+    const resetPasswordToken = crypto
+      .createHash('sha256')
+      .update(req.params.resettoken)
+      .digest('hex');
 
     const user = await User.findOne({
       resetPasswordToken,
@@ -172,7 +172,6 @@ const resetPassword = async (req, res, next) => {
       return res.status(400).json({ message: 'אסימון איפוס סיסמה לא חוקי או שפג תוקפו.' });
     }
 
-    // Set new password
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(req.body.password, salt);
     user.resetPasswordToken = undefined;
